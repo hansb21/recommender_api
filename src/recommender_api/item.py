553a1b2dbfa975e7cebe733063c27ed61b265ea7
Hans Buss <hans.buss@mailfence.com>
2023-10-18 14:46:07 -0300

    Refatoração das funções relacionadas a itens para utilização de json separados

--- src/recommender_api/item.py
+++ src/recommender_api/item.py
@@ -4,49 +4,38 @@ import json
 from flask import abort
 
 CONTEXT, USER = utils.open_files()
-
+ITEM = utils.open_files("item")
 
 def create(item: dict) -> None | tuple:
     if item["Context"] in CONTEXT.keys():
+        if item["Context"] not in ITEM.keys():
+            ITEM[item["Context"]] = {}
+            ITEM[item["Context"]]["items"] = {}
+
         for i in item["itemIds"]:
-            print(i)
-            if i["itemId"] not in CONTEXT[item["Context"]]["items"].keys():
-                CONTEXT[item["Context"]]["items"][i["itemId"]] = {}
+            if i["itemId"] not in ITEM[item["Context"]]["items"].keys():
+                ITEM[item["Context"]]["items"][i["itemId"]] = {}
             for key, value in i.items():
-                CONTEXT[item["Context"]]["items"][i["itemId"]][key] = value
+                ITEM[item["Context"]]["items"][i["itemId"]][key] = value
 
-    utils.save_files("context", CONTEXT)
+    utils.save_files("item", ITEM)
 
-
-def delete(Context: str, userId: str, itemId: str) -> None | tuple:
-    if Context in CONTEXT.keys():
-        if itemId in CONTEXT[Context]["items"].keys():
-            if userId in CONTEXT[Context]["items"][itemId]["action"]:
-                CONTEXT[Context]["items"][itemId]["action"]["users"].pop(userId)
-                CONTEXT[Context]["items"][itemId]["action"]["interactions"] -= 1
-
-            else:
-                abort(422, f"Unprocessable Entity - User {userId} dosen't exists")
+# Necessário ser refeita e refatorada para seguir o padrão
+def delete(Context: str, itemId: str) -> None | tuple:
+    if Context in CONTEXT.keys() and Context in ITEM.keys():
+        if itemId in ITEM[Context]["items"].keys():
+            ITEM[Context]["items"].pop(itemId)
         else:
             abort(422, f"Unprocessable Entity - Item {itemId} dosen't exists")
     else:
         abort(422, f"Unprocessable Entity - Context {Context}dosen't exists")
 
-    if len(CONTEXT[Context]["items"][itemId]["action"]) == 0:
-        CONTEXT[Context]["items"].remove(itemId)
 
-    if userId in USER.keys():
-        if itemId in USER[userId].keys() and USER[userId]["Context"] == Context:
-            del USER[userId][itemId]
-    else:
-        abort(422, f"Unprocessable Entity - User {userId} dosen't exists")
-
-    utils.save_files("context", CONTEXT)
-    utils.save_files("user", USER)
+    utils.save_files("action", ITEM)
     return (200, "Sucessfully deleted item")
 
 
-def deleteHistory(Context: str, userId: str) -> None | tuple:
-    for itemId in CONTEXT[Context]["items"]:
-        delete(Context, userId, itemId)
-    return (200, "Sucessfully deleted user history")
+#ef deleteHistory(Context: str, userId: str) -> None | tuple:
+#   for itemId in CONTEXT[Context]["items"]:
+#       delete(Context, itemId)
+#   return (200, "Sucessfully deleted user history")
